bcb87384ac8455dbcfb63f840afd49ce576caa22
vmontazeri <vmontazery@gmail.com>
2021-10-03 19:15:57 -0700

    bug fix

--- includes/new_exp.m
+++ includes/new_exp.m
@@ -7,6 +7,13 @@ h = uicontrol('Style', 'text', 'String','Please wait for new instruction before 
     'Units','Norm', 'Position', [ 0 0.55 1 0.05 ]);
 
 h = uicontrol('Style','pushbutton','String','Proceed', 'FontSize', 25, ...
-    'Units', 'Norm', 'Position', [0 0.15 1 0.2 ], 'Callback',  @(btn,event) ButtonPushed(temp));
+    'Units', 'Norm', 'Position', [0 0.15 1 0.2 ], 'Callback',  @(btn,event) ButtonPushed());
+
+uiwait(fig1)
+
+ function ButtonPushed()
+        uiresume(fig1)
+        close all
+ end
 
 end
